f7417477f2d258abb69ba568ed85cad75639f1bf
Kartik Kankurte <kartikkankurte@gmail.com>
2022-05-23 23:18:05 +0530

    clean up the workspace

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBsnxyftzGcjBOFZC3uIrVC-TRvne3pyS0",
+    authDomain: "snapkart-4b965.firebaseapp.com",
+    projectId: "snapkart-4b965",
+    storageBucket: "snapkart-4b965.appspot.com",
+    messagingSenderId: "232777010200",
+    appId: "1:232777010200:web:ed92d4cf7f3f68b25c076d",
+    measurementId: "G-R4EXWJEPQW"
+  };
