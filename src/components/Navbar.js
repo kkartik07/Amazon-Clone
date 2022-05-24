d0f01c3d32449aa4428f625ca4bd937e8dbf3923
Kartik Kankurte <kartikkankurte@gmail.com>
2022-05-24 18:43:41 +0530

    modified navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,41 @@ import React from 'react'
 import '../stylesheets/Navbar.css'
 import Logo from '../icons/logo.png'
 import Search from '../icons/search.png'
+import ShoppingBasket from '../icons/shopping-basket.png'
 
 function Navbar() {
     return (
         <div className='navbar'>
             <img className='navbar-logo' alt='logo' src={Logo} />
+
             <span className='navbar-brandname'>SnapKart</span>
             <div className='navbar-search'>
                 <input className='navbar-search-input' type='text' />
                 <img src={Search} className='navbar-searcher' alt='search-icon' />
             </div>
 
-            <div className='navbar-options'> 
+            <div className='navbar-options'>
                 <div className='navbar-option'>
-                    <span>Hello Kartik!</span>
-                    <span>Sign In</span>
+                    <span className='navbar-option-line1'>Hello Kartik!</span>
+                    <span className='navbar-option-line2'>Sign In</span>
                 </div>
 
                 <div className='navbar-option'>
-                    <span>Returns</span>
-                    <span> & Orders</span>
+                    <span className='navbar-option-line1'>Returns</span>
+                    <span className='navbar-option-line2'> & Orders</span>
                 </div>
 
                 <div className='navbar-option'>
-                    <span>Your Cart</span>
+                    <span className='navbar-option-line1'>Your Cart</span>
+                    <img src={ShoppingBasket} alt='shopping-basket' className='shopping-basket-icon' />
+                    <span className="header__optionLineTwo header__basketCount">
+                        0
+                    </span>
                 </div>
+
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar
