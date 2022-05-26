5b38fa4cf3fd10e8cbe0bfeb98546d040eaafff8
Kartik Kankurte <kartikkankurte@gmail.com>
2022-05-26 23:56:47 +0530

    make login backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import '../stylesheets/Login.css'
 import logo from '../icons/logo-big.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { auth } from '../Firebase.js'
 
 function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                navigate('/')
+            })
+            .catch(error => alert(error.message))
+    }
+    const register = (e) => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    navigate('/')
+                }
+            })
+            .catch(error => alert(error.message))
+
+    }
     return (
         <div className='login'>
             <Link to='/' className='login-logo'>
@@ -16,19 +46,19 @@ function Login() {
                 <h1>Sign-in</h1>
                 <form>
                     <h5>E-Mail</h5>
-                    <input type='text' className='input' />
+                    <input type='text' className='input' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' className='input' />
+                    <input type='password' className='input' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className='login-signin-btn'>Sign In</button>
+                    <button type='submit' className='login-signin-btn' onClick={signIn}>Sign In</button>
 
                 </form>
                 <p className='login-notice'>By signing in you agree to SnapKart's terms and conditions of use & sale. Please check our privacy notice, our cookies notice and our Interest-based Ads notice.</p>
-                <button className='login-register-btn'>Create an account</button>
+                <button type='submit' className='login-register-btn' onClick={register} redirect='/'>Create an account</button>
             </div>
 
-        </div>
+        </div >
     )
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBsnxyftzGcjBOFZC3uIrVC-TRvne3pyS0",
-    authDomain: "snapkart-4b965.firebaseapp.com",
-    projectId: "snapkart-4b965",
-    storageBucket: "snapkart-4b965.appspot.com",
-    messagingSenderId: "232777010200",
-    appId: "1:232777010200:web:ed92d4cf7f3f68b25c076d",
-    measurementId: "G-R4EXWJEPQW"
-  };
+  apiKey: "AIzaSyBsnxyftzGcjBOFZC3uIrVC-TRvne3pyS0",
+  authDomain: "snapkart-4b965.firebaseapp.com",
+  projectId: "snapkart-4b965",
+  storageBucket: "snapkart-4b965.appspot.com",
+  messagingSenderId: "232777010200",
+  appId: "1:232777010200:web:ed92d4cf7f3f68b25c076d",
+  measurementId: "G-R4EXWJEPQW"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
